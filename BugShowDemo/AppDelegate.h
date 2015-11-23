8f6c30d2cd427b66defa075f52ada78062968cad
Soul <laigb133@126.com>
2015-11-23 08:55:40 +0800

    Initial Commit

---
+++ BugShowDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BugShowDemo
+//
+//  Created by zhyy on 15/11/23.
+//  Copyright © 2015年 Soul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BugShowDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BugShowDemo
+//
+//  Created by zhyy on 15/11/23.
+//  Copyright © 2015年 Soul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
